c276f26356a74d5d47a86e6efd805f045280fc7f
opu183059 <opuctg440@gmail.com>
2023-08-26 22:06:30 +0600

    Initial setup completed. Tailwind added and Navbar added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./components/navbar/Navbar";
+
+const App = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,78 @@
+import logo from "../../assets/images/logo.png";
+import { FiSearch } from "react-icons/fi";
+import { BiLockOpenAlt } from "react-icons/bi";
+import { RiGroupLine } from "react-icons/ri";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { RxCross1 } from "react-icons/rx";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+  const menu = [
+    { name: "home", link: "#" },
+    { name: "about us", link: "#" },
+    { name: "product", link: "#" },
+    { name: "flash sales", link: "#" },
+  ];
+
+  return (
+    <div>
+      <div className="navbar max-w-6xl border-b-[2px] py-3 mx-auto flex justify-between items-center px-2 md:px-0">
+        <div className="logo flex items-center">
+          <img src={logo} alt="logo" className="w-[50px]" />
+          <h1 className="font-kaushan text-[30px]">Boutiquin</h1>
+        </div>
+        <div className="menu hidden md:block">
+          <ul className="flex gap-4">
+            {menu.map((item) => (
+              <li key={item.name}>
+                <a
+                  href={item.link}
+                  className="uppercase font-poppins hover:text-pink-600   duration-150"
+                >
+                  {item.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="right flex gap-3">
+          <BiLockOpenAlt size={20}></BiLockOpenAlt>
+          <RiGroupLine size={20}></RiGroupLine>
+          <FiSearch size={20}></FiSearch>
+          <button
+            className="md:hidden"
+            onClick={() => {
+              setToggle(!toggle);
+            }}
+          >
+            {toggle ? (
+              <RxCross1 size={20}></RxCross1>
+            ) : (
+              <HiOutlineMenuAlt3 size={20}></HiOutlineMenuAlt3>
+            )}
+          </button>
+        </div>
+      </div>
+
+      {/* Mobile menu  */}
+      <div
+        className={`mobileMenu overflow-hidden ${
+          toggle ? `h-80` : `h-0`
+        } bg-white duration-1000 flex justify-center items-center text-center z-50`}
+      >
+        <ul className="ps-3 py-4">
+          {menu.map((item) => (
+            <li key={item.name} className="mb-3">
+              <a href={item.link} className="uppercase font-poppins">
+                {item.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
